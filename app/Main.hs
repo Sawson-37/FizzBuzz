9e551c9fbe3dcdc5e436187ac6380b16710bd5ba
Sawson-37 <haskell.sawson@gmail.com>
2021-08-04 15:42:55 +0900

    grepの実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,117 @@
-import System.IO
+ -- module Main where
+
+import System.Directory (listDirectory, doesFileExist)
 import Text.Read(readMaybe)
+import System.IO
+import System.FilePath(takeExtension ,(</>))
+import System.Environment(getArgs)
+import Data.List(isInfixOf)
 
--- FizzBuzz判定
-fzBz :: Int -> IO()
-fzBz num 
-          | fzFlag && bzFlag = print "FizzBuzz"
-          | fzFlag = print "Fizz"
-          | bzFlag =  print "Buzz"
-          | otherwise =  print num
-          where 
-            fzFlag = num `mod` 3 == 0
-            bzFlag = num `mod` 5 == 0
-
- 
-readInt :: String -> Maybe Int -- 標準入力を数値に変換するための苦肉の案
-readInt = readMaybe
-
-main :: IO ()
+main :: IO()
 main = do
-      r <- readInt <$> getLine
-      case r of
-          Just r' -> fzBz r'
-          otherwise -> print "error"
+  (str:_) <- getArgs
 
--- 順番(1～入力という指定があったので)
--- 文字列に対応(これじゃあ答え合わせ...)
--- 勝敗決まってからループ出る
+  contents <- getContents 
+  putStrLn $ grepPure str contents
 
+grepPure :: String -> String -> String
+grepPure str contents = unlines $ filter isContainsStr $ lines contents 
+ where isContainsStr :: String -> Bool
+       isContainsStr line = str `isInfixOf` line 
+
+countLine :: String -> IO Int -- 大本の
+countLine filePath = do
+
+  isExsit <- doesFileExist filePath -- 指しているPathがfileかの確認
+  if isExsit
+      then if ".hs" == takeExtension filePath -- fileであれば“.has”のファイルか`
+         then do
+            str <- readFile filePath -- Pathのfileをreadfileで読む　
+            return $ length $ lines str -- 文字列をくっつけて、長さを返す
+         else return 0 -- 異なれば０行として返す
+      else do
+         paths <- listDirectory filePath
+         let absPaths = map toAbsPath paths -- IO以外で返ってくるとき
+         ns <- mapM countLine absPaths -- IOで返ってくるとき
+         return $ sum ns
+ where
+   toAbsPath :: String -> String
+   toAbsPath file = filePath </> file
+{-
+
+
+
+import System.Directory (listDirectory, doesFileExist)
+import Text.Read(readMaybe)
+import System.IO
+import System.FilePath(takeExtension ,(</>))
+
+Prerude
+-- getLIne
+-- putStr
+-- putStrLn
+-- print
+-- readFile
+-- writeFile
+
+system.directory
+--listDIrectory
+
+
+system.filepath
+
+ioは基本的に自分で定義できない
+isSuffiXOf <- 
+
+fizzbazz、COUNTLINES 復讐すること
+
+1
+
+
+
+-}
+
+{-
+import System.Directory (listDirectory, doesFileExist)
+import Text.Read(readMaybe)
+import System.IO
+import System.FilePath(takeExtension ,(</>))
+
+Prerude
+-- getLIne
+-- putStr
+-- putStrLn
+-- print
+-- readFile
+-- writeFile
+
+system.directory
+--listDIrectory
+
+-}
+
+countLine2 :: String -> IO Int
+countLine2 filePath = do
+   isExsit <- doesFileExist filePath
+   if isExsit 
+      then do
+         if ".hs" == takeExtension filePath
+            then do 
+               str <-　readFile filePath 
+               return $ length $ lines str
+            else return 0 
+         else do
+            pathList <- listDirectory filePath
+            let absPathlist = map toAbs pathList  
+            ns <- mapM countLine2 absPathlist
+            return $ sum ns
+ where
+  toAbs :: String -> String 
+  toAbs file =  filePath </> file
+
+
+{-
+
+
+
+-}
